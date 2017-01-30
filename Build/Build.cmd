0de3ff9d9bd99b1e7907615283acb2495b8f2df0
0xFireball <0xFireball@outlook.com>
2017-01-30 10:37:32 -0800

    update old build script

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-%windir%\microsoft.net\framework\v4.0.30319\msbuild ..\Confuser2.sln /p:Configuration=Release "/p:Platform=Any CPU"
+%windir%\microsoft.net\framework\v4.0.30319\msbuild ..\ModPhuserEx.sln /p:Configuration=Release "/p:Platform=Any CPU"
 
 IF %ERRORLEVEL% NEQ 0 GOTO err
 
